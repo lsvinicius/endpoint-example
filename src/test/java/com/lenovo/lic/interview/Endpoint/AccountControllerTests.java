92324368257ca8c4eb147cd9d49c27e4d58dd71d
vinicius <vinicius.lopes.silva89@gmail.com>
2017-07-02 13:06:05 -0300

    Added a test to validate the user, however it isn't working. This URL is secured and for some strange reason, it is not giving access to user 'novo', that would be normal if this user wasn't already registered in the database (which is not the case)

--- src/test/java/com/lenovo/lic/interview/Endpoint/AccountControllerTests.java
+++ src/test/java/com/lenovo/lic/interview/Endpoint/AccountControllerTests.java
@@ -1,8 +1,8 @@
 package com.lenovo.lic.interview.Endpoint;
 
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.Before;
@@ -10,7 +10,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.codec.Base64;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -22,12 +27,18 @@ import com.lenovo.lic.interview.Endpoint.model.Account;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ContextConfiguration(classes={EndpointApplication.class})
 public class AccountControllerTests {
 	@Autowired
 	private WebApplicationContext context;
 
 	private MockMvc mvc;
 
+
+    @MockBean
+    UserDetailsService userDetailsService;
+
+	
 	@Before
 	public void setup() {
 		mvc = MockMvcBuilders
@@ -36,18 +47,36 @@ public class AccountControllerTests {
 				.build();
 	}
 	
-	@Test
-	public void testRegister() throws Exception {
-		Account account = new Account("novo", "novo");
+	private MvcResult registerAccount(Account account) throws Exception {
 		Gson gson = new Gson();
 		MvcResult result = this.mvc.perform(post("/account/register").contentType(MediaType.APPLICATION_JSON)
 							.content(gson.toJson(account)))
 						    .andExpect(status().isOk())
-						    .andReturn();
+						    .andReturn();		
+		return result;
+	}
+	
+	@Test
+	public void testRegister() throws Exception {
+		Account account = new Account("novo", "novo");
+		MvcResult result = registerAccount(account);
 		String content = result.getResponse().getContentAsString();
 		String contentCopy = "{\"success\":true,\"message\":\"account registered successfully\","
 				+ "\"user\":\"novo\"}" + 
 				"";
 		assert content.equals(contentCopy);
+	}
+	
+	@Test
+	public void testValidate() throws Exception {
+		
+		MvcResult result = this.mvc.perform(get("/account/validate")
+						   .with(SecurityMockMvcRequestPostProcessors.httpBasic("novo","novo"))
+						   .contentType(MediaType.APPLICATION_JSON))
+						   .andExpect(status().isOk()).andReturn();
+				
+		String content = result.getResponse().getContentAsString();
+		System.out.println("MVCRESULT "+result);
+		assert 1 == 0;
 	}
 }
