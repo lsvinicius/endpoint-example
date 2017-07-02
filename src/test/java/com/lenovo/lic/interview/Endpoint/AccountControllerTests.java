03930137c0bad85e9c6fb81e2e5b4c6e24636672
vinicius <vinicius.lopes.silva89@gmail.com>
2017-07-02 11:05:58 -0300

    Finally added a communication channel between controller and MocMvc, however, it is using production database. Searching for a fix to that

--- src/test/java/com/lenovo/lic/interview/Endpoint/AccountControllerTests.java
+++ src/test/java/com/lenovo/lic/interview/Endpoint/AccountControllerTests.java
@@ -1,31 +1,47 @@
 package com.lenovo.lic.interview.Endpoint;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-
-import com.lenovo.lic.interview.Endpoint.controller.AccountController;
-import com.lenovo.lic.interview.Endpoint.repository.AccountRepository;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(AccountController.class)
-@ContextConfiguration(classes={AccountRepository.class})
+@ContextConfiguration
+@WebAppConfiguration
+@SpringBootTest
 public class AccountControllerTests {
-	 @Autowired
-	 private MockMvc mvc;
+	@Autowired
+	private WebApplicationContext context;
 
-	 @Test
-	 public void testExample() throws Exception {
-		 this.mvc.perform(post("/account/register").accept(MediaType.APPLICATION_JSON))
-	     .andExpect(status().isOk()).andExpect(content().string("Honda Civic"));
-	 }
+	private MockMvc mvc;
+
+	@Before
+	public void setup() {
+		mvc = MockMvcBuilders
+				.webAppContextSetup(context)
+				.apply(springSecurity())
+				.build();
+	}
+	
+	@Test
+	public void testExample() throws Exception {
+		this.mvc.perform(post("/account/register").contentType(MediaType.APPLICATION_JSON)
+		.content("{\"username\": \"novo\", \"password\":\"novo\"}"))
+	    .andExpect(status().isOk()).andExpect(content().string("Honda Civic"));
+	}
 }
